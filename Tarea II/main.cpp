35344e95577cd2fafe388d76d9fcefbfdfd7b232
Guest <guest-ukozlh@ecci-lbl-105-01.ecci.ucr.ac.cr>
2018-04-27 16:19:39 -0600

    Generacion de lista de tamaño n con números aleatorios entre [0,2n]

--- Tarea II/main.cpp
+++ Tarea II/main.cpp
@@ -1,46 +1,39 @@
 #include <cstdlib>
+#include <time.h>
+
 #include "llist.h"
 #include "bstree.h"
 
-typedef int DataType;
+typedef long int Integer;
+void printList(const llist<Integer> &list);
 
-void printList(const llist<DataType> &list);
+int main(int argc, char** argv) {
 
-int main(int argc, char** argv)
-{
-	llist<DataType> * list = new llist<DataType>();
-	llnode<DataType> * element = new llnode<DataType>(5, nullptr, nullptr);
-	llnode<DataType> * element2 = new llnode<DataType>(-1, nullptr, nullptr);
-	llnode<DataType> * element3 = new llnode<DataType>(30, nullptr, nullptr);
-	list->listInsert(element);
-	list->listInsert(element2);
-	list->listInsert(element3);
+    srand(time(NULL));
+    Integer n = 100000000;
+    Integer mulFactor = 2 * n;
+    llist<Integer> list;
 
-	printList(*list);
+    for (Integer i = 0; i < n; ++i) {
 
-
-	llist<DataType> * list2 = new llist<DataType>(*list);
-	printList(*list2);
-
-	delete list;
-	delete list2;
-	delete element;
-	delete element2;
-
-	return 0;
+        llnode<Integer> * node = new llnode<Integer>((rand() % mulFactor), nullptr, nullptr);
+        list.listInsert(node);
+        node = nullptr;
+    }
+    return 0;
 }
 
-void printList(const llist<DataType> &list)
-{
-	llnode<DataType> * p = new llnode<DataType>();
-	p = list.getNil()->next;
-	int cantNodos = 0;
+void printList(const llist<Integer> &list) {
+    llnode<Integer> * p = new llnode<Integer>();
+    p = list.getNil()->next;
+    int cantNodos = 0;
 
-	while (p != list.getNil()) {
-		std::cout << "Nodo " << ++cantNodos << ": " << p->key << std::endl << std::endl;
-		p = p->next;
-	}
-	std::cout << std::endl << std::endl;
+    while (p != list.getNil()) {
+        std::cout << "Nodo " << ++cantNodos << ": " << p->key << std::endl << std::endl;
+        p = p->next;
+    }
+    std::cout << std::endl << std::endl;
+    p = nullptr;
 }
 
 
