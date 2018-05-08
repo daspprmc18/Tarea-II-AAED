54fa0f42ac6e56bbe0f6259ea0684ace9183fef7
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2018-05-08 00:46:47 +0000

    Midiendo tiempos de ejecución y contando número de búsquedas, usando std::chrono ....

--- Tarea II/main.cpp
+++ Tarea II/main.cpp
@@ -1,5 +1,7 @@
+#include <chrono>
 #include <cstdlib>
 #include <iomanip>
+#include <random>
 #include <stack>
 #include <time.h>
 
@@ -7,27 +9,28 @@
 #include "llist.h"
 
 typedef long int Integer;
+typedef std::mt19937 RandomNumberGenerator;
+
 void printList(const llist<Integer> &list);
 void printStack(stack<Integer> &stk);
 
 int main(int argc, char** argv) {
 
     int option = 1;
+    Integer n = 100;
+    Integer mulFactor = (2 * n) - 1;
 
-    srand(time(NULL));
-    Integer n = 1000000;
-    Integer mulFactor = 2 * n;
-
-    for (int j = 0; j < 10; ++j)
-        std::cout << rand() % mulFactor << "\n\n";
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count(); // Reloj del sistema.
+    RandomNumberGenerator generator(seed); // Generador 
+    std::uniform_int_distribution<uint32_t> distribution(0, mulFactor); // Rango [0 2n[.
 
     // Código de prueba para Lista enlazada.
     if (!option) {
 
         /*
          *  Inserte en una lista vacía n = 1 000 000 de nodos cuyas llaves sean enteros seleccio-
-         *  nados aleatoriamente en el rango [0, 2n). Seleccione elementos al azar en el mismo
-         *  rango [0, 2n) y búsquelos en la lista (estén o no en ella) registrando el número de
+         *  nados aleatoriamente en el rango [0, 2n[. Seleccione elementos al azar en el mismo
+         *  rango [0, 2n[ y búsquelos en la lista (estén o no en ella) registrando el número de
          *  búsquedas realizadas en un lapso de diez segundos.
          */
         {
@@ -36,23 +39,22 @@ int main(int argc, char** argv) {
 
             for (Integer i = 0; i < n; ++i) {
 
-                randomNode = new llnode<Integer>((rand() % mulFactor), nullptr, nullptr);
+                randomNode = new llnode<Integer>(distribution(generator), nullptr, nullptr);
                 randomList.listInsert(randomNode);
                 randomNode = nullptr;
             }
 
-            printList(randomList);
             return 0;
 
             for (int i = 0; i < 10; ++i) {
 
-                randomList.listSearch((rand() % mulFactor));
+                randomList.listSearch(distribution(generator));
             }
         }
 
         /*
          *  Inserte en una lista vacía las llaves 0, 1, ..., n − 1, en ese orden. Seleccione elementos
-         *  al azar en el rango [0, 2n), y registre el número de búsquedas que se logró hacer en
+         *  al azar en el rango [0, 2n[, y registre el número de búsquedas que se logró hacer en
          *  un lapso de 10 segundos. 
          */
         {
@@ -68,7 +70,7 @@ int main(int argc, char** argv) {
 
             for (int i = 0; i < 10; ++i) {
 
-                sequentialList.listSearch((rand() % mulFactor));
+                sequentialList.listSearch(distribution(generator));
             }
         }
 
@@ -80,23 +82,44 @@ int main(int argc, char** argv) {
          */
     } else { // Código de prueba para Árbol binario.
 
-        tree<Integer> randomTree;
-        node<Integer> * randomNode = nullptr;
+        { // Árbol Binario Búsqueda: Elementos aleatorios.
+            tree<Integer> randomTree;
+            node<Integer> * randomNode = nullptr;
 
-        for (Integer i = 0; i < n; ++i) {
-            randomNode = new node<Integer>((rand() % mulFactor), nullptr, nullptr, nullptr);
-            randomTree.treeInsert(randomNode);
-            randomNode = nullptr;
+            for (Integer i = 0; i < n; ++i) {
+                randomNode = new node<Integer>(distribution(generator), nullptr, nullptr, nullptr);
+                randomTree.treeInsert(randomNode);
+                randomNode = nullptr;
+            }
+
+            Integer count = 0;
+            std::chrono::seconds elapsed(0);
+            std::chrono::system_clock::time_point finish;
+            std::chrono::system_clock::time_point start = std::chrono::system_clock::now();
+
+            while (elapsed.count() < 10) {
+
+                randomTree.treeSearch(distribution(generator));
+                ++count;
+                finish = std::chrono::system_clock::now();
+                elapsed = std::chrono::duration_cast<std::chrono::seconds>(finish - start);
+            }
+
+            std::cout << "Tiempo transcurrido: " << elapsed.count() << " segundos\n\n";
+            std::cout << "Número de búsquedas realizadas: " << count << "\n\n";
         }
 
-        /*tree<Integer> sequentialTree;
-        node<Integer> * sequentialNode = nullptr;
+        { // Árbol Binario Búsqueda: Elementos secuenciales.
+            tree<Integer> sequentialTree;
+            node<Integer> * sequentialNode = nullptr;
 
-        for (Integer i = 0; i < n; ++i) {
-            sequentialNode = new node<Integer>(i, nullptr, nullptr, nullptr);
-            sequentialTree.treeInsert(sequentialNode);
-            sequentialNode = nullptr;
-        }*/
+            for (Integer i = 0; i < n; ++i) {
+
+                sequentialNode = new node<Integer>(i, nullptr, nullptr, nullptr);
+                sequentialTree.treeInsert(sequentialNode);
+                sequentialNode = nullptr;
+            }
+        }
     }
 
     return 0;
@@ -109,6 +132,7 @@ void printList(const llist<Integer> &list) {
     int cantNodos = 0;
 
     while (p != list.getNil()) {
+
         std::cout << "Nodo " << ++cantNodos << ": " << p->key << std::endl << std::endl;
         p = p->next;
     }
