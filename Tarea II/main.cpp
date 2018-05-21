5c4f48b695c84805a68481908e1984727a694105
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2018-05-21 15:39:09 +0000

    POR ALGULA RAZÓN SI EJECUTO LA BÚSQUEDA SECUENCIAL DEL ÁRBOL ROJINEGRO ALEATORIO ANTES QUE EL SECUENCIAL LA CANTIDAD DE BÚSQUEDAS SE REDUCE SIGNIFICATIVAMENTE..... WTF

--- Tarea II/main.cpp
+++ Tarea II/main.cpp
@@ -36,10 +36,9 @@ int main(int argc, char** argv) {
     std::mt19937 engine(std::chrono::system_clock::now().time_since_epoch().count()); // Generador de números aleatorios "Mersenne Twister 19937"
     std::uniform_int_distribution<Integer> distribution(min, max); // Rango distribución uniforme: [0,2n-1].
 
-    // testRandomHashT(engine, distribution, *(np));
-
-    //testRandomRedBlackTree(engine, distribution, n);
+    testRandomHashT(engine, distribution, *(np));
     testSequientialRedBlackTree(engine, distribution, n); // Si lo corro solo hace más búsquedas que cuando corrí previamente la búsqueda de aleatorios...
+    testRandomRedBlackTree(engine, distribution, n);
 
     return 0;
 }
@@ -139,7 +138,7 @@ void testRandomHashT(std::mt19937 &engine, std::uniform_int_distribution<Integer
     for (Integer i = 0; i < n; ++i)
         hashTable.insert( distribution(engine) );
 
-    cout << "Factor de carga: " << hashTable.loadFactor() << "\n\n";
+    //cout << "Factor de carga: " << hashTable.loadFactor() << "\n\n";
 
     std::cout << "----> Tabla de Dispersión Aleatoria <----\n\n";
     Integer count = 0;
